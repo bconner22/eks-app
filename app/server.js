08d021f6afeaac3005589b79ed35ccb04d4765fd
Brian Conner <bconner22@gmail.com>
2020-08-07 21:40:08 +0000

    Getting unix time setup

--- app/server.js
+++ app/server.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const express = require('express');
-
+const { exec } = require("child_process");
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
+const date = Math.floor(new Date() / 1000)
 app.get('/', (req, res) => {
-  return res.json({ message:  'Automate all the things!' });
+  return res.json({ message:  'Automate all the things!',
+                    timestamp: date });
 });
 
 app.listen(PORT, HOST);
